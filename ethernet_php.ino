acdd131ef2f7835ed78cb56125e8f5be5fd1b263
yoonsoo94 <daivan35@gmail.com>
2017-02-06 16:04:43 +0900

    수정2

--- ethernet_php.ino
+++ ethernet_php.ino
@@ -1,50 +1,49 @@
 #include <Ethernet.h>
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };  
-byte ip[] = { 192, 168, 83, 12 };
-byte gw[] = { 192, 168, 83, 1  };
+byte ip[] = { 192, 168, 123, 152 };
+byte gw[] = { 192, 168, 123, 254  };
 byte subnet[] = { 255, 255, 255, 0 };
 // numeric IP for Raspberry Web Server
-byte server[] = { 192, 168, 83, 13 };
+byte server[] = { 192, 168, 123, 116 };
 
-int data = 0;
-int tempPin = 5;  // In This case the temperature is taken from pin 2 and sent to a sql server
+EthernetClient client;
 
-void setup()
-{
+int gasdata = 0;
+int tempPin = 5;
+
+void setup(){
   pinMode(tempPin, INPUT);
+  Ethernet.begin(mac, ip, gw, subnet);
   Serial.begin(9600);               // Used for serial debugging
+  Serial.println("Program running...");
 }
 
-void loop()
-{
-  Serial.println("Program running...");
-  delay(5000);
+void loop(){
+  delay(1000);
   senddata();                                 // Data is sent every 5 seconds
 }
 
-void senddata()
-{
-    data = analogRead(tempPin);           //Reading analog data
-    
-    Ethernet.begin(mac, ip, gw, subnet);
-    EthernetClient client;
-    Serial.println();
-    Serial.println("Forbinder?");
-    delay(1000);                                    //Keeps the connection from freezing
+void senddata(){
+    gasdata = analogRead(tempPin);           //Reading analog data    
+    delay(1000);                                    //Keeps the connection from freezing  
     
     if (client.connect(server, 80)) {
       Serial.print("Connected: ");
-      client.print("GET http://192.168.137.17/script.php?=gas");
-      client.print(data);
+      client.print("GET http://192.168.137.17/index.php?gas=");
+//      client.print("GET /index.php?gas=");
+//      client.print("GET /?gas=");
+      client.print(gasdata);
       client.println(" HTTP/1.1");
-      client.println("Host: 192.168.83.13");
+      client.println("Host: 192.168.123.116");
       client.println();
-      Serial.println(data);
+      Serial.println(gasdata);
+      Serial.println();
     }
       
     else{
       Serial.println("Connection unsuccesfull");
     }
+
 
     //stop client
     client.stop();
